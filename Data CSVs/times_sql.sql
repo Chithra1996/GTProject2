16865a23efd6ecc5a6ee165009ab9eb2bec727be
William Powell <80594473+wtpowell@users.noreply.github.com>
2021-07-18 13:49:35 -0400

    updated times csv and times sql creation

---
+++ Data CSVs/times_sql.sql
@@ -0,0 +1,18 @@
+CREATE TABLE times (
+		world_rank VARCHAR(30),               
+		university_name VARCHAR(100),   
+		country VARCHAR(50),                    
+		teaching DECIMAL,             
+		international DECIMAL,            
+		research DECIMAL,                 
+		citations DECIMAL,                
+		income DECIMAL,                   
+		total_score DECIMAL,             
+		num_students VARCHAR(20),             
+		student_staff_ratio DECIMAL, 
+		year varchar(10),
+		int_students DECIMAL,     
+		female_male DECIMAL         
+		);
+		
+SELECT * FROM times
